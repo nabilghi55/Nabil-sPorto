165deccc902bebad356f8f41cbc145fc84086499
billgyi <nabilghi.fitacademy@gmail.com>
2024-05-17 17:02:18 +0700

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <div className="flex pt-20 lg:pt-0 lg:flex-row flex-col-reverse w-full lg:h-[85vh]  h-fit mx-auto   items-center ">
       <div className="">
         <h1 className="text-3xl text-center lg:text-left font-bold text-blue-900 dark:text-gray-400">
-          Hello Everyone 👋, i'm
+          Hello Everyone 👋, i am
         </h1>
         <h1 className="dark:text-gray-100 text-center  lg:text-left text-[#7089c7] font-bold lg:text-5xl text-2xl mt-4">
           M Nabil Ghifari Monteswi
